c918c4d93c31428443a34c6c04fae9ed0522494d
Hiago Henrique <hiago_h3@hotmail.com>
2022-11-01 18:28:49 -0300

    Parametros web concluídos

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,22 @@
-
-
-export default function Home() {
-  return (
-    <h1>Hello World!</h1>
-  )
+interface Homeprops {
+  count: number;
 }
+
+export default function Home(props: Homeprops) {
+ 
+  return (
+    <h1>Quantidade de bolões: {props.count}</h1>
+  )
+};
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  return {
+    props: {
+      count: data.count,
+    }
+  }
+}
+
